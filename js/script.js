8078dda104c300891c654905b0d27d6b9d6a446e
unknown <gyebnarpetter@gmail.com>
2020-10-07 15:53:37 +0200

    about-us

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
-// Nav slidetoggle
+// Navigation functions
 $(".nav-btn").click( () => {
     $("nav").slideToggle()
 });
 
-// Nav scroll effect
 window.onscroll = () => {
     let headerWrapper = document.querySelector(".header-wrapper");
     let nav = document.querySelector("nav");
@@ -16,3 +15,42 @@ window.onscroll = () => {
         nav.style.top = "100px";
     }
 }
+
+// About us pager
+let calypsoDescription = {
+    branding: `<p>We're a close team of creatives, designers & developers who work together to create beautiful, engaging digital experiences. Our team is specially designed to create successful, custom, innovative digital products from the initial concept through full product launch. From whiteboard sketches to pixel-perfect designs.</p>
+    <p>We take pride in delivering only the best. We're an agency born out of a passion to make great products. We help companies and startups from all over the world materialize their ideas into great projects.</p>`,
+
+    development: `<p>Lorem We're a close team of creatives, designers & developers who work together to create beautiful, engaging digital experiences. Our team is specially designed to create successful, custom, innovative digital products from the initial concept through full product launch. From whiteboard sketches to pixel-perfect designs.</p>
+    <p>We take pride in delivering only the best. We're an agency born out of a passion to make great products. We help companies and startups from all over the world materialize their ideas into great projects.</p>`,
+
+    photography: `<p>We're a close team of creatives, designers & developers who work together to create beautiful, engaging digital experiences. Our team is specially designed to create successful, custom, innovative digital products from the initial concept through full product launch. From whiteboard sketches to pixel-perfect designs.</p>
+    <p>We take pride in delivering only the best. We're an agency born out of a passion to make great products. We help companies and startups from all over the world materialize their ideas into great projects.</p>`
+};
+
+let aboutUs = document.querySelector(".about-us-description"),
+    aboutUsLinks = document.querySelectorAll(".about-us-buttons a"),
+    branding = document.querySelector(".branding"),
+    development = document.querySelector(".development"),
+    photography = document.querySelector(".photography");
+
+aboutUs.innerHTML = calypsoDescription.branding;
+
+let changeText = (id) => {
+    aboutUs.style.opacity = 0;
+    setTimeout(() => {
+        aboutUs.innerHTML = calypsoDescription[`${id}`];
+        aboutUs.style.opacity = 1;
+    }, 700);
+}
+
+$(".about-us-buttons a").click( function(event) {
+    event.preventDefault();
+    let id = $(this).attr("class");
+    let element = document.querySelector(`.${id}`);
+    $(".about-us-active").removeClass("about-us-active");
+    element.classList.add("about-us-active");
+
+    changeText(id);
+});
+    
